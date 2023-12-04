0f20c9ee25ac08ba306f5ef713cda5bb8f18d85a
Jake Kealey <jake.kealey@nhs.net>
2023-12-04 09:35:35 +0000

    Version to use new tables in test db

--- docs/fasterSUS_sanity_check_new_tables.sql
+++ docs/fasterSUS_sanity_check_new_tables.sql
@@ -1,22 +1,22 @@
 -- Sanity check: Run on latest month to see live figures for activity. Typically expect 6000+ rows
 
-SELECT year, Fin_Month, COUNT(1) FROM ncl.dbo.Maindata_FasterSUS_IP_all_93c ip
+SELECT fin_year, fin_month, COUNT(1) FROM [Data_Lab_NCL_Test].[Current].[maindata_fs_ip] ip
 
 --Check for closed spell
 INNER JOIN [Data_Store_SUS_Unified].[APC].[spell] fss
-ON ip.eID = fss.PRIMARYKEY_ID
+ON ip.primary_id = fss.PRIMARYKEY_ID
 
 --Check for Dominant episode
 INNER JOIN [Data_Store_SUS_Unified].[APC].[spell.episodes] fsse
-ON ip.eID = fsse.PRIMARYKEY_ID
+ON ip.primary_id = fsse.PRIMARYKEY_ID
 
 
-WHERE year = '2023/24' and ProviderCode = 'RAL'
+WHERE fin_year = '2023/24' and provider_code = 'RAL'
 AND POD IN ('DC', 'EL')
 --Filter on closed spells and the dominant episode
 AND fss.[spell.open_spell_indicator] = 0
 AND fsse.[dominant_episode_flag] = 1
 
-GROUP BY year, Fin_Month
+GROUP BY fin_year, Fin_Month
 
 ORDER BY Fin_Month
