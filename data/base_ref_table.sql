c6b584fcc3834fc64dbcd057405ca37c1bded70e
Jake Kealey <jake.kealey@nhs.net>
2023-12-04 12:11:54 +0000

    Change to join on hospital number and org code

--- data/base_ref_table.sql
+++ data/base_ref_table.sql
@@ -2,24 +2,20 @@
 -- Outputs basic procedure identifying information
 SELECT opcs.*, 
 	CONCAT(opcs_1, opcs_2, opcs_3, opcs_4, opcs_5, opcs_6, opcs_7, opcs_8,
-		opcs_9, opcs_10, opcs_11, opcs_12, opcs_13, opcs_14, opcs_15, opcs_16,
-		opcs_17, opcs_18, opcs_19, opcs_20, opcs_21, opcs_22, opcs_23, opcs_24) AS opcs_all,
+		opcs_9, opcs_10, opcs_11, opcs_12) AS opcs_all,
 
 	icd_1, icd_2, icd_3, icd_4, icd_5, icd_6, icd_7, icd_8,
-	icd_9, icd_10, icd_11, icd_12, icd_13, icd_14, icd_15, icd_16,
-	icd_17, icd_18, icd_19, icd_20, icd_21, icd_22, icd_23, icd_24,
+	icd_9, icd_10, icd_11, icd_12,
 
 	CONCAT(icd_1, icd_2, icd_3, icd_4, icd_5, icd_6, icd_7, icd_8,
-		icd_9, icd_10, icd_11, icd_12, icd_13, icd_14, icd_15, icd_16,
-		icd_17, icd_18, icd_19, icd_20, icd_21, icd_22, icd_23, icd_24) AS icd_all
+		icd_9, icd_10, icd_11, icd_12) AS icd_all
 FROM (
 	SELECT 
 		Year, Fin_Month, org_code, Age, POD, tfc, main_spec,
 		hospital_no,
 		PRIMARYKEY_ID, EPISODES_ID,
 		opcs_1, opcs_2, opcs_3, opcs_4, opcs_5, opcs_6, opcs_7, opcs_8,
-		opcs_9, opcs_10, opcs_11, opcs_12, opcs_13, opcs_14, opcs_15, opcs_16,
-		opcs_17, opcs_18, opcs_19, opcs_20, opcs_21, opcs_22, opcs_23, opcs_24
+		opcs_9, opcs_10, opcs_11, opcs_12
 	FROM (
 		SELECT 
 			fs.Year, fs.Fin_Month,
@@ -33,14 +29,21 @@ FROM (
 
 		FROM [Data_Store_SUS_Unified].[APC].[spell.episodes.clinical_coding.procedure.opcs] opcs
 
-		LEFT JOIN [NCL].[dbo].[Maindata_FasterSUS_IP_all_93c] fs
-		ON fs.eID = opcs.PRIMARYKEY_ID
+		--To get hospital spell to join with fs table
+		INNER JOIN [Data_Store_SUS_Unified].[APC].[spell] fss
+		ON opcs.PRIMARYKEY_ID = fss.PRIMARYKEY_ID
 
+		--Main row info
+		LEFT JOIN [NCL].[dbo].[Maindata_FasterSUS_IP_all_93c] fs
+		ON fs.SLAM_HospitalNo = fss.[spell.hospital_provider_spell_number]
+		AND fs.ProviderCode = fss.[spell.commissioning.service_agreement.provider_derived]
+
+		--Episode data
 		LEFT JOIN [Data_Store_SUS_Unified].[APC].[spell.episodes] fsse
 		ON opcs.PRIMARYKEY_ID = fsse.PRIMARYKEY_ID
 		AND opcs.EPISODES_ID = fsse.EPISODES_ID
 
-		WHERE OPCS_ID <= 24
+		WHERE OPCS_ID <= 12
 		AND Year IS NOT NULL
 		AND fsse.[spell.open_spell_indicator] = 0
 		AND fsse.[dominant_episode_flag] = 1
@@ -51,8 +54,7 @@ FROM (
 		MAX(code)
 		FOR OPCS_ID IN (
 			opcs_1, opcs_2, opcs_3, opcs_4, opcs_5, opcs_6, opcs_7, opcs_8,
-			opcs_9, opcs_10, opcs_11, opcs_12, opcs_13, opcs_14, opcs_15, opcs_16,
-			opcs_17, opcs_18, opcs_19, opcs_20, opcs_21, opcs_22, opcs_23, opcs_24
+			opcs_9, opcs_10, opcs_11, opcs_12
 		)
 	) AS unpvt
 ) opcs
@@ -62,8 +64,7 @@ LEFT JOIN (
 		Year, Fin_Month, Age, POD,
 		PRIMARYKEY_ID, EPISODES_ID,
 		icd_1, icd_2, icd_3, icd_4, icd_5, icd_6, icd_7, icd_8,
-		icd_9, icd_10, icd_11, icd_12, icd_13, icd_14, icd_15, icd_16,
-		icd_17, icd_18, icd_19, icd_20, icd_21, icd_22, icd_23, icd_24
+		icd_9, icd_10, icd_11, icd_12
 	FROM (
 		SELECT 
 			fs.Year, fs.Fin_Month,
@@ -73,15 +74,14 @@ LEFT JOIN (
 		FROM [Data_Store_SUS_Unified].[APC].[spell.episodes.clinical_coding.diagnosis.icd] icd
 		LEFT JOIN [NCL].[dbo].[Maindata_FasterSUS_IP_all_93c] fs
 		ON fs.eID = icd.PRIMARYKEY_ID
-		WHERE ICD_ID <= 24
+		WHERE ICD_ID <= 12
 		AND YEAR IS NOT NULL
 	) AS s
 	PIVOT (
 		MAX(code)
 		FOR ICD_ID IN (
 			icd_1, icd_2, icd_3, icd_4, icd_5, icd_6, icd_7, icd_8,
-			icd_9, icd_10, icd_11, icd_12, icd_13, icd_14, icd_15, icd_16,
-			icd_17, icd_18, icd_19, icd_20, icd_21, icd_22, icd_23, icd_24
+			icd_9, icd_10, icd_11, icd_12
 		)
 	) AS unpvt
 ) icd
